827e688f346a982e1c29ec76ecbdbfa75b8c6c6e
RJ Cunanan <rjlugtuc@gmail.com>
2024-08-15 12:30:14 -0700

    Added styles and modified App.js to fetch user information from an API.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from "./robots";
+import './App.css';
 
 
 class App extends Component {
+
+    // Order that React functions will run here:
+    // 1. constructor()
+    // 2. render()
+    // 3. componentDidMount()
+    // 4. render()
+
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
+    }
+
+    // This is part of React so we don't need arrow functons here
+    componentDidMount() {
+        // fetch() is a method from the window object
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
     }
 
     // NOTE: When creating your own functions in App, use arrow functions => so that
@@ -23,13 +38,17 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
     }
 }
     
